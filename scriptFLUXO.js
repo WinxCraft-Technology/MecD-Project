11dafb121d1f4601fdbde6456d2f111f5070de7f
Matheus Silva <msantossilva005@gmail.com>
2023-05-22 18:26:20 -0300

    Script do Modal

---
+++ scriptFLUXO.js
@@ -0,0 +1,8 @@
+// JavaScript para manipular o modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
