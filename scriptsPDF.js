2f603ee73a9d73b4a3391d7269aa570523c8f05c
Matheus Silva <msantossilva005@gmail.com>
2023-06-01 15:28:36 -0300

    Segunda idéia de script

--- scriptsPDF.js
+++ scriptsPDF.js
@@ -273,3 +273,122 @@ select2.addEventListener("change", atualizarLista);
 select3.addEventListener("change", atualizarLista);
 select4.addEventListener("change", atualizarLista);
 */ 
+
+
+
+
+
+
+
+
+
+
+
+
+/* Segunda Solução
+
+function iniciarBanco() {
+  const firebaseConfig = {
+    apiKey: "SUA_API_KEY",
+    authDomain: "mecd-project.firebaseapp.com",
+    projectId: "mecd-project",
+    storageBucket: "mecd-project.appspot.com",
+    messagingSenderId: "SEU_SENDER_ID",
+    appId: "SEU_APP_ID",
+    measurementId: "SEU_MEASUREMENT_ID"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  exibirDocumentos();
+}
+
+function exibirDocumentos() {
+  const db = firebase.firestore();
+  const divResultado = document.getElementById("resultado");
+  divResultado.innerHTML = "";
+
+  db.collection("filtros")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        const label = document.createElement("label");
+        label.textContent = doc.id;
+        divResultado.appendChild(label);
+
+        const select = document.createElement("select");
+        var id = doc.id.replace(" ", "_");
+        select.id = id;
+        select.className = "filtro-select";
+        divResultado.appendChild(select);
+
+        db.collection("filtros")
+          .doc(doc.id)
+          .get()
+          .then((docSnapshot) => {
+            if (docSnapshot.exists) {
+              const data = docSnapshot.data();
+              Object.keys(data).forEach((key) => {
+                if (key !== "nome") {
+                  const option = document.createElement("option");
+                  option.value = data[key];
+                  option.textContent = data[key];
+                  select.appendChild(option);
+                }
+              });
+            }
+          })
+          .catch((error) => {
+            console.error("Error getting document:", error);
+          });
+      });
+    })
+    .catch((error) => {
+      console.error("Error getting documents:", error);
+    });
+}
+
+async function atualizarLista() {
+  const selects = document.querySelectorAll('.filtro-select');
+  const filtrosSelecionados = Array.from(selects).map((select) => ({
+    campo: select.id.replace("_", " "),
+    valor: select.value
+  }));
+
+  const db = firebase.firestore();
+  const mecanismosRef = db.collection("mecanismos");
+
+  let filtroFinal = [];
+
+  if (filtrosSelecionados.length > 0) {
+    let query = mecanismosRef;
+
+    filtrosSelecionados.forEach((filtro) => {
+      if (filtro.valor !== "N") {
+        query = query.where(filtro.campo, "==", filtro.valor);
+      }
+    });
+
+    const querySnapshot = await query.get();
+    querySnapshot.forEach((doc) => {
+      filtroFinal.push(doc.id);
+    });
+  }
+
+  lista.innerHTML = "";
+  filtroFinal.forEach((valor) => {
+    const option = document.createElement("option");
+    option.innerText = valor;
+    lista.appendChild(option);
+  });
+
+  const numPDF = document.getElementById("lista");
+  const attNumPDF = numPDF.getElementsByTagName("option").length;
+  document.getElementById("num-pdf").innerHTML = "Nº de Arquivos: " + attNumPDF;
+}
+
+select.addEventListener("change", atualizarLista);
+select2.addEventListener("change", atualizarLista);
+select3.addEventListener("change", atualizarLista);
+select4.addEventListener("change", atualizarLista);
+*/
