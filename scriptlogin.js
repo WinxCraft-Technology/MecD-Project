79972d54545f8729e88cd93b26b9bfb669e2220b
Matheus Silva <msantossilva005@gmail.com>
2023-06-05 12:09:27 -0300

    Resolvendo bug de redirecionamento

--- scriptlogin.js
+++ scriptlogin.js
@@ -1,20 +1,24 @@
 function gerarToken() {
-    var timestamp = new Date().getTime(); // Obtém o timestamp atual
-    var token = "token_" + timestamp; // Gera um token usando o timestamp
-    return token;
+  var timestamp = new Date().getTime(); // Obtém o timestamp atual
+  var token = "token_" + timestamp; // Gera um token usando o timestamp
+  return token;
 }
 
-function isAutenticado() { 
-    var token = localStorage.getItem("token");
-    return token !== null && token.startsWith("token_");
+function isAutenticado() {
+  var token = localStorage.getItem("token");
+  return token !== null && token.startsWith("token_");
 }
 
 // Verifica se o usuário está autenticado.
 if (!isAutenticado()) {
+  if (window.location.href.indexOf("ADMlogin.html") === -1) {
     window.location.href = "ADMlogin.html";
+  }
 }
 
-document.getElementById("loginForm").addEventListener("submit", function(event) {
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault(); // Impede o envio padrão do formulário.
 
     // Obtendo os valores do formulário,
@@ -26,14 +30,10 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     var passwordP = "administracaomecdpaulo";
 
     if (username === usernameP && password === passwordP) {
-        var token = gerarToken();
-        localStorage.setItem("token", token);
-        window.location.href = "adm.html";
+      var token = gerarToken();
+      localStorage.setItem("token", token);
+      window.location.href = "adm.html";
     } else {
-        alert("Nome de usuário ou senha inválidos!");
+      alert("Nome de usuário ou senha inválidos!");
     }
-});
-
-
-
-
+  });
